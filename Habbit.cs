62e69167191b898a50dab65203be5d38789ebc03
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-04-18 21:48:36 +0200

    feat: create Habbit model

---
+++ Habbit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace HabitLogger
+{
+    internal class Habbit
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private int Quantity { get; set; }
+        private DateTime Date { get; set; }
+    }
+}
