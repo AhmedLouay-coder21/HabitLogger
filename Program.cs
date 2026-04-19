199bd6f1c543153cc81ce81b192c543fd27ac4d5
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-04-19 23:27:56 +0200

    UI:clear console on every time the program is opened

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace HabitLogger
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             displayAppTitle();
             using var db = new HabitDb();
             db.Database.EnsureCreated();
